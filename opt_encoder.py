3858abf035d912691c029bfc376f00362d87963c
Elias Augusto <38014178+EAugustoAnalysis@users.noreply.github.com>
2019-08-16 19:26:00 -0400

    Add files via upload

--- opt_encoder.py
+++ opt_encoder.py
@@ -9,13 +9,19 @@ from colorama import Fore,Back,Style
 import re
 from textwrap import wrap
 
-def onesub(b,bc):
+def onesub(b,bc,o):
 	s = Solver()
-	bad_chars = [ 0x20, 0x80, 0x0A, 0x0D, 0x2F, 0x3A, 0x3F, 0x2E, 0x40 ]
+	bad_chars = [ 0x00, 0x80, 0x0A, 0x0D]
 	bad_chars+=bc #I added ability to specify additional badchars
+	if not o:
+		extrabad=[0x20,0x2F,0x3A,0x3F,0x2E,0x40]
+		bad_chars+=extrabad
 	x = BitVec('x', 32)
 	for k in range(0, 32, 8):
-		s.add(Extract(k+7, k, x) > BitVecVal(0x20, 8))
+		if not o:
+			s.add(Extract(k+7, k, x) > BitVecVal(0x20, 8))
+		if o:
+			s.add(Extract(k+7, k, x) > BitVecVal(0x00, 8))
 		s.add(ULT(Extract(k+7, k, x), BitVecVal(0x80, 8)))
 		for c in bad_chars:
 			s.add(Extract(k+7, k, x) != BitVecVal(c, 8))
@@ -30,16 +36,22 @@ def onesub(b,bc):
 			r.append(s.model()[i].as_long())
 	return r
 
-def twosub(b,bc):
+def twosub(b,bc,o):
 	s = Solver()
-	bad_chars = [ 0x20, 0x80, 0x0A, 0x0D, 0x2F, 0x3A, 0x3F, 0x2E, 0x40 ]
+	bad_chars = [ 0x00, 0x80, 0x0A, 0x0D]
 	bad_chars+=bc #I added ability to specify additional badchars
+	if not o:
+		extrabad=[0x20,0x2F,0x3A,0x3F,0x2E,0x40]
+		bad_chars+=extrabad
 	x, y = BitVecs('x y', 32)
 	variables = [x, y]
 
 	for var in variables:
 		for k in range(0, 32, 8):
-			s.add(Extract(k+7, k, var) > BitVecVal(0x20, 8))
+			if not o:
+				s.add(Extract(k+7, k, var) > BitVecVal(0x20, 8))
+			if o:
+				s.add(Extract(k+7, k, x) > BitVecVal(0x00, 8))
 			s.add(ULT(Extract(k+7, k, var), BitVecVal(0x80, 8)))
 			for c in bad_chars:
 				s.add(Extract(k+7, k, var) != BitVecVal(c, 8))
@@ -55,16 +67,22 @@ def twosub(b,bc):
 	
 	return r
 	
-def threesub(b,bc):
+def threesub(b,bc,o):
 	s = Solver()
-	bad_chars = [ 0x20, 0x80, 0x0A, 0x0D, 0x2F, 0x3A, 0x3F, 0x2E, 0x40 ]
+	bad_chars = [ 0x00, 0x80, 0x0A, 0x0D]
 	bad_chars+=bc #I added ability to specify additional badchars
+	if not o:
+		extrabad=[0x20,0x2F,0x3A,0x3F,0x2E,0x40]
+		bad_chars+=extrabad
 	x, y, z = BitVecs('x y z', 32)
 	variables = [x, y, z]
 
 	for var in variables:
 		for k in range(0, 32, 8):
-			s.add(Extract(k+7, k, var) > BitVecVal(0x20, 8))
+			if not o:
+				s.add(Extract(k+7, k, var) > BitVecVal(0x20, 8))
+			if o:
+				s.add(Extract(k+7, k, x) > BitVecVal(0x00, 8))
 			s.add(ULT(Extract(k+7, k, var), BitVecVal(0x80, 8)))
 			for c in bad_chars:
 				s.add(Extract(k+7, k, var) != BitVecVal(c, 8))
@@ -79,43 +97,49 @@ def threesub(b,bc):
 	
 	return r
 
-def solve(b,bc): #Optimized version of BUGTREE's function that sub encodes 32 bit hex addresses in 0xFFFFFFFF format
-	var=onesub(b,bc)
+def solve(b,bc,o): #Optimized version of BUGTREE's function that sub encodes 32 bit hex addresses in 0xFFFFFFFF format
+	var=onesub(b,bc,o)
 	r=[]
 	if var[0]==False:
-		var=twosub(b,bc)
+		var=twosub(b,bc,o)
 		if var[0]==False:
-			var=threesub(b,bc) #threesub automatically kills program if bad
+			var=threesub(b,bc,o) #threesub automatically kills program if bad
 	if var[0]==1: #If twosub or onesub works	
 		del var[0]
 	r+=var
 	return r
 
-def normalize(bc): #I tried to adapt this into a normalizer
-    s = Solver()
-    bad_chars = [ 0x20, 0x80, 0x0A, 0x0D, 0x2F, 0x3A, 0x3F, 0x2E, 0x40 ]
-    bad_chars+=bc #I added ability to specify additional badchars
-    x, y = BitVecs('x y', 32)
-    variables = [x, y]
+def normalize(bc,o): #I tried to adapt this into a normalizer
+	s = Solver()
+	bad_chars = [ 0x00, 0x80, 0x0A, 0x0D]
+	bad_chars+=bc #I added ability to specify additional badchars
+	if not o:
+		extrabad=[0x20,0x2F,0x3A,0x3F,0x2E,0x40]
+		bad_chars+=extrabad
+	x, y = BitVecs('x y', 32)
+	variables = [x, y]
 
-    for var in variables:
-        for k in range(0, 32, 8):
-            s.add(Extract(k+7, k, var) > BitVecVal(0x20, 8))
-            s.add(ULT(Extract(k+7, k, var), BitVecVal(0x80, 8)))
-            for c in bad_chars:
-                s.add(Extract(k+7, k, var) != BitVecVal(c, 8))
+	for var in variables:
+		for k in range(0, 32, 8):
+			if not o:
+				s.add(Extract(k+7, k, var) > BitVecVal(0x20, 8))
+			if o:
+				s.add(Extract(k+7, k, x) > BitVecVal(0x00, 8))
+			s.add(ULT(Extract(k+7, k, var), BitVecVal(0x80, 8)))
+			for c in bad_chars:
+				s.add(Extract(k+7, k, var) != BitVecVal(c, 8))
 
-    s.add(x&y==0)
+	s.add(x&y==0)
 
-    if not s.check()==sat: #Added error handling because it didn't previously exist
-        print(Fore.GREEN+"Normalizer incompatible with badchars, consider using the -a flag to enable sub based normalizer or use a custom normalizer (-n)")
-        exit(0);
-    s.model()
-    r = []
-    for i in s.model():
-        r.append(s.model()[i].as_long())
+	if not s.check()==sat: #Added error handling because it didn't previously exist
+		print(Fore.GREEN+"Normalizer incompatible with badchars, consider using the -a flag to enable sub based normalizer or use a custom normalizer (-n)")
+		exit(0);
+	s.model()
+	r = []
+	for i in s.model():
+		r.append(s.model()[i].as_long())
 
-    return r
+	return r
 
 parser = argparse.ArgumentParser() #Argument that takes shellcode 
 parser.add_argument("-s", "--shellcode", type=str,
@@ -134,6 +158,8 @@ parser.add_argument("-e", "--espsetup", type=str,
                     help="Automatically sets up the stack for you. Format: -e \"0x[current ESP address], 0x[intended ESP address]\". ASLR safe, compatible with relocatable stacks.")
 parser.add_argument("-m", "--mlgen", action="store_true",
                     help="Generates hex shellcode in \"\\xff\\xff\\xff...\" format.")
+parser.add_argument("-o", "--override", action="store_true",
+                    help="Removes most badchars, makes all ASCII characters available. 0x0d, 0x0a, 0x00, and anything above 0x7F remain badchars. Specify any additional badchars yourself. Use at your own risk.")
 args = parser.parse_args()
 
 if not args.shellcode: #Exit if no shellcode given
@@ -147,7 +173,10 @@ if args.pad:
 		while len(scode)%8!=0:
 			scode+="90"
 	else:
-		parser.error("Malformed or invalid machine language")	
+		parser.error("Malformed or invalid machine language")
+override=False
+if args.override: #Allows override of badchars
+	override=True	
 		
 if len(scode)%8!=0: #Exit if shellcode length is less than 4
 	parser.error("Shellcode byte length is not a multiple of 4, pad shellcode or use -p flag and retry.")
@@ -157,12 +186,12 @@ if args.badchars:
 	bcharstxt=args.badchars.split(",")
 	bdchars+=[int(x,16) for x in bcharstxt]
 
-nres=[0,0]
-if (not args.normalizer) and (not args.altnorm): #Normalizer setup
-	nres=normalize(bdchars) #No need to do extra math if they're using a custom normalizer
-
 if args.mlgen and args.normalizer:
 	parser.error("Cannot assemble shellcode when custom normalizer is in use.")
+
+nres=[0,0]
+if (not args.normalizer) and (not args.altnorm): #Normalizer setup
+	nres=normalize(bdchars,override) #No need to do extra math if they're using a custom normalizer
 	
 if args.altnorm and args.normalizer:
 	parser.error("Cannot use two normalizers.")
@@ -225,6 +254,8 @@ if args.badchars:
 	buffer+=";Custom badchars: "+args.badchars+"\n\n"
 if args.normalizer:
 	buffer+=";Custom eax normalizer in use, check if errors occur\n"
+if args.override:
+	buffer+=";Badchar override in effect\n"
 if not args.espsetup:	
 	buffer+=";Note: You still need to set up the stack yourself, you do not have the -e flag on.\n\n"
 buffer+="global _start\n_start:\n\n"
@@ -234,7 +265,7 @@ if args.espsetup:
 	espstart=int(sotext[0],16)
 	espend=int(sotext[1],16)
 	soffset=hex((espstart-espend)&(2**32-1)) #Give us the offset we need to subtract from the stack
-	espoff=solve(int(soffset,16),bdchars) #Find the subtraction instructions that could give us the stack offset we want
+	espoff=solve(int(soffset,16),bdchars,override) #Find the subtraction instructions that could give us the stack offset we want
 	
 	sumCheck=0
 	for h in espoff[-len(espoff):]:
@@ -249,7 +280,7 @@ if args.espsetup:
 			buffer+=norm[g]
 			buffer+="\n"
 	elif args.altnorm:
-		norm=solve(int("0x0",16),bdchars)
+		norm=solve(int("0x0",16),bdchars,override)
 		for h in norm[-len(norm):]:
 			buffer+="sub eax,"+hex(h)+" ;normalize eax for safety\n"
 	else:
@@ -275,7 +306,7 @@ for i in range(0,len(reciporical)): #Assembly output
 				buffer+=norm[g]
 				buffer+="\n"
 		elif args.altnorm:
-			norm=solve(int("0x0",16),bdchars)
+			norm=solve(int("0x0",16),bdchars,override)
 			for h in norm[-len(norm):]:
 				buffer+="sub eax,"+hex(h)+" ;normalize eax\n"
 		else:
@@ -284,7 +315,7 @@ for i in range(0,len(reciporical)): #Assembly output
 		buffer+="push eax\n\n"
 	else:
 		sumCheck=0
-		result=solve(int(reciporical[i],16),bdchars)
+		result=solve(int(reciporical[i],16),bdchars,override)
 		for h in result[-len(result):]:
 			sumCheck+=h
 		checksum=hex(sumCheck & (2**32-1))  #remove annoying extra byte that sometimes appears and does not effect acccuracy
@@ -297,7 +328,7 @@ for i in range(0,len(reciporical)): #Assembly output
 				buffer+=norm[g]
 				buffer+="\n"
 		elif args.altnorm:
-			norm=solve(int("0x0",16),bdchars)
+			norm=solve(int("0x0",16),bdchars,override)
 			for h in norm[-len(norm):]:
 				buffer+="sub eax,"+hex(h)+" ;normalize eax\n"
 		else:
@@ -311,6 +342,8 @@ if not args.file:
 
 def hexforml(val):
 	vh=hex(val) #convrt to hex
+	if len(vh)==9: #insert a 0 if the first byte is 0
+		vh=vh[:2]+'0'+vh[2:]
 	vhs=vh[2:] #remove '0x'
 	vhr="".join(map(str.__add__,vhs[-2::-2],vhs[-1::-2])) #A more consise way to reverse every two bytes than I was previously using
 	return "\\x"+"\\x".join(a+b for a,b in zip(vhr[::2],vhr[1::2])) #Insert a \x before every character
@@ -323,10 +356,10 @@ if args.mlgen:
 		espstart=int(sotext[0],16)
 		espend=int(sotext[1],16)
 		soffset=hex((espstart-espend)&(2**32-1)) #Give us the offset we need to subtract from the stack
-		espoff=solve(int(soffset,16),bdchars) #Find the subtraction instructions that could give us the stack offset we want
+		espoff=solve(int(soffset,16),bdchars,override) #Find the subtraction instructions that could give us the stack offset we want
 		
 		if args.altnorm:
-			norm=solve(int("0x0",16),bdchars)
+			norm=solve(int("0x0",16),bdchars,override)
 			for h in norm[-len(norm):]:
 				mlbuffer+="\\x2d"+hexforml(h) #sub
 		else:
@@ -344,7 +377,7 @@ if args.mlgen:
 	for i in range(0,len(reciporical)): #Assembly output
 		if precip[i]=='0x0':
 			if args.altnorm:
-				norm=solve(int("0x0",16),bdchars)
+				norm=solve(int("0x0",16),bdchars,override)
 				for h in norm[-len(norm):]:
 					mlbuffer+="\\x2d"+hexforml(h) #sub
 			else:
@@ -352,9 +385,9 @@ if args.mlgen:
 				mlbuffer+="\\x25"+hexforml(nres[1])
 			mlbuffer+="\\x50" #push eax
 		else:
-			result=solve(int(reciporical[i],16),bdchars)
+			result=solve(int(reciporical[i],16),bdchars,override)
 			if args.altnorm:
-				norm=solve(int("0x0",16),bdchars)
+				norm=solve(int("0x0",16),bdchars,override)
 				for h in norm[-len(norm):]:
 					mlbuffer+="\\x2d"+hexforml(h) #sub
 			else:
